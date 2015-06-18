f311f23315727ed198ea0cc02b6c755c3da0f1dd
Jordan Deyton <deytonjh@ornl.gov>
2015-06-18 12:39:31 -0400

    Added missing licenses.
    Signed-off-by: Jordan Deyton <deytonjh@ornl.gov>

--- src/org.eclipse.ice.datastructures/src/org/eclipse/ice/datastructures/componentVisitor/SelectiveComponentVisitor.java
+++ src/org.eclipse.ice.datastructures/src/org/eclipse/ice/datastructures/componentVisitor/SelectiveComponentVisitor.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jordan Deyton (UT-Battelle, LLC.) - initial API and implementation and/or 
+ *    initial documentation
+ *    Alex McCaskey (UT-Battelle, LLC.) - added EMF Component classes
+ *    Jay Jay Billings (UT-Battelle, LLC.) - removed BatteryComponent
+ *    Jay Jay Billings (UT-Battelle, LLC.) - added ListComponent
+ *    Jordan Deyton (UT-Battelle, LLC.) - fixed generic type warnings
+ *******************************************************************************/
 package org.eclipse.ice.datastructures.componentVisitor;
 
 import org.eclipse.ice.datastructures.ICEObject.ListComponent;
