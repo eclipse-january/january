db45c8b35159818296a8edbe414faf160f62fc09
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-11-23 13:07:35 +0000

    Add dynamic load interface

---
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/io/ILazyDynamicLoader.java
@@ -0,0 +1,16 @@
+/*-
+ * Copyright 2015 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.dawnsci.analysis.api.io;
+
+public interface ILazyDynamicLoader {
+
+	public int[] refreshShape();
+	
+}
