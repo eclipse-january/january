83bf663a5405b03966b24855ecd302687bacd094
Jacob Filik <jacob.filik@diamond.ac.uk>
2015-12-09 13:47:58 +0000

    Add dictionary of dimensions so lazy dataset can be related to dims

--- org.eclipse.dawnsci.analysis.dataset/src/org/eclipse/dawnsci/analysis/dataset/metadata/AxesMetadataImpl.java
+++ org.eclipse.dawnsci.analysis.dataset/src/org/eclipse/dawnsci/analysis/dataset/metadata/AxesMetadataImpl.java
@@ -11,7 +11,9 @@ package org.eclipse.dawnsci.analysis.dataset.metadata;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.dawnsci.analysis.api.dataset.ILazyDataset;
 import org.eclipse.dawnsci.analysis.api.metadata.AxesMetadata;
@@ -25,23 +27,29 @@ public class AxesMetadataImpl implements AxesMetadata {
 	@Reshapeable(matchRank = true)
 	@Sliceable
 	final List<ILazyDataset>[] allAxes;
+	
+	final Map<ILazyDataset,int[]> dimensionMap;
 
 	@SuppressWarnings("unchecked")
 	public AxesMetadataImpl(int rank) {
 		allAxes = new List[rank];
+		dimensionMap = new HashMap<ILazyDataset,int[]>();
 	}
 
 	@SuppressWarnings("unchecked")
 	public AxesMetadataImpl(AxesMetadataImpl axesMetadataImpl) {
 		int r = axesMetadataImpl.allAxes.length;
 		allAxes = new List[r];
+		dimensionMap = new HashMap<ILazyDataset,int[]>();
 		for (int i = 0; i < r; i++) {
 			List<ILazyDataset> ol = axesMetadataImpl.allAxes[i];
 			if (ol == null)
 				continue;
 			List<ILazyDataset> list = new ArrayList<ILazyDataset>();
 			for (ILazyDataset l : ol) {
-				list.add(l == null ? null : l.getSliceView());
+				ILazyDataset lv = l == null ? null : l.getSliceView();
+				list.add(lv);
+				if (axesMetadataImpl.dimensionMap.containsKey(l)) dimensionMap.put(lv, axesMetadataImpl.dimensionMap.get(lv).clone());
 			}
 			allAxes[i] = list;
 		}
@@ -51,7 +59,7 @@ public class AxesMetadataImpl implements AxesMetadata {
 	public void setAxis(int axisDim, ILazyDataset... axisData) {
 		ArrayList<ILazyDataset> axisList = new ArrayList<ILazyDataset>(0);
 		for (int i = 0; i < axisData.length; i++) {
-			axisList.add(sanitizeAxisData(axisDim, axisData[i]));
+			axisList.add(sanitizeAxisData(axisData[i],axisDim));
 		}
 		allAxes[axisDim] = axisList;
 	}
@@ -88,26 +96,47 @@ public class AxesMetadataImpl implements AxesMetadata {
 		if (allAxes[axisDim] == null) {
 			allAxes[axisDim] = new ArrayList<ILazyDataset>();
 		}
-		allAxes[axisDim].add(sanitizeAxisData(axisDim, axisData));
+		allAxes[axisDim].add(sanitizeAxisData(axisData,axisDim));
+	}
+	
+	public void addAxis(ILazyDataset axisData, int... axisDim) {
+		if (allAxes[axisDim[0]] == null) {
+			allAxes[axisDim[0]] = new ArrayList<ILazyDataset>();
+		}
+		
+		ILazyDataset lz = sanitizeAxisData(axisData,axisDim);
+		allAxes[axisDim[0]].add(lz);
+		if (lz != null) dimensionMap.put(lz, axisDim);
+	}
+	
+	public int[] getDimensions(ILazyDataset lz) {
+		return dimensionMap.get(lz);
 	}
 
-	private ILazyDataset sanitizeAxisData(int axisDim, ILazyDataset axisData) {
+	private ILazyDataset sanitizeAxisData(ILazyDataset axisData, int... axisDim) {
 		// remove any axes metadata to prevent infinite recursion
 		// and also check rank
 		if (axisData == null) return null;
-		ILazyDataset view = axisData.getSliceView();
-		view.clearMetadata(AxesMetadata.class);
-		int r = axisData.getRank(); 
-		if (r != allAxes.length) {
-			if (r > 1) {
-				throw new IllegalArgumentException("Given axis dataset must be zero or one dimensional, or match rank");
+		
+		if (axisDim.length == 1) {
+			int ad = axisDim[0];
+			ILazyDataset view = axisData.getSliceView();
+			view.clearMetadata(AxesMetadata.class);
+			int r = axisData.getRank(); 
+			if (r != allAxes.length) {
+				if (r > 1) {
+					throw new IllegalArgumentException("Given axis dataset must be zero or one dimensional, or match rank");
+				}
+				int[] newShape = new int[allAxes.length];
+				Arrays.fill(newShape, 1);
+				newShape[ad] = axisData.getSize();
+				view.setShape(newShape);
 			}
-			int[] newShape = new int[allAxes.length];
-			Arrays.fill(newShape, 1);
-			newShape[axisDim] = axisData.getSize();
-			view.setShape(newShape);
+			return view;
 		}
-		return view;
+		
+		return null;
+		
 	}
 
 	@Override
