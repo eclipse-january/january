b5342c66a3f45db2acf246ca5c66680cad130f75
amccaskey <mccaskeyaj@ornl.gov>
2016-01-14 15:51:14 -0500

    Implemented FileEntry and its test.
    Signed-off-by: amccaskey <mccaskeyaj@ornl.gov>

--- org.eclipse.ice.datastructures/src/org/eclipse/ice/datastructures/entry/FileEntry.java
+++ org.eclipse.ice.datastructures/src/org/eclipse/ice/datastructures/entry/FileEntry.java
@@ -1,28 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2014- UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Initial API and implementation and/or initial documentation - Jay Jay Billings, 
+ *    Jordan H. Deyton, Dasha Gorin, Alexander J. McCaskey, Taylor Patterson, 
+ *    Claire Saunders, Matthew Wang, Anna Wojtowicz
+ *     
+ *******************************************************************************/
 package org.eclipse.ice.datastructures.entry;
 
 import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ice.datastructures.ICEObject.IUpdateable;
+import org.eclipse.ice.datastructures.ICEObject.Identifiable;
 
+/**
+ * FileEntry is a subclass of the DiscreteEntry that keeps track of 
+ * a list of files (as allowed values) for the user to select. The 
+ * value for this IEntry is the selected file name. 
+ * 
+ * @author Alex McCaskey
+ *
+ */
+@XmlRootElement(name = "FileEntry")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class FileEntry extends DiscreteEntry {
 
+	/**
+	 * Reference to the selected file. 
+	 */
+	@XmlTransient
 	private IFile file;
+	
+	/**
+	 * Reference to the IProject space that the Item 
+	 * containing this IEntry belongs to. This is used 
+	 * to query information about available files to list 
+	 * as allowed values. 
+	 */
+	@XmlTransient
 	private IProject project;
+	
+	/**
+	 * The file extension this FileEntry cares about. If 
+	 * provided, only files with this extension will be added 
+	 * to the list of allowed values. 
+	 */
+	@XmlAttribute
 	private String fileExtension;
 
+	/**
+	 * The Constructor
+	 */
 	public FileEntry() {
 		super();
+		fileExtension = "";
 	}
 
+	/**
+	 * The constructor, with ability to specify file types 
+	 * clients care about for this FileEntry
+	 * @param fileType
+	 */
 	public FileEntry(String fileType) {
 		super();
 		fileExtension = fileType;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ice.datastructures.entry.DiscreteEntry#clone()
+	 */
 	@Override
 	public Object clone() {
 		FileEntry entry = new FileEntry();
@@ -30,14 +92,26 @@ public class FileEntry extends DiscreteEntry {
 		return entry;
 	}
 
+	/**
+	 * This operation lets clients specify the IProject space 
+	 * for the Item containing this FileEntry. 
+	 * 
+	 * @param projectSpace The project this Entry belongs to.
+	 */
 	public void setProject(IProject projectSpace) {
 		project = projectSpace;
 		generateAllowedValues();
 		isModified = true;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ice.datastructures.entry.DiscreteEntry#setValue(java.lang.String)
+	 */
 	@Override
 	public boolean setValue(String newValue) {
+		// Set the value as normal for a Discrete entry, 
+		// but also get a reference to the file.
 		if (super.setValue(newValue)) {
 			file = project.getFile(newValue);
 			return true;
@@ -45,10 +119,27 @@ public class FileEntry extends DiscreteEntry {
 		return false;
 	}
 	
+	/**
+	 * Return the absolute file path for the selected file. 
+	 * 
+	 * @return path
+	 */
 	public String getAbsoluteFilePath() {
 		return file.getLocation().toOSString();
 	}
 	
+	/**
+	 * Public operation that lets clients update the list of files, eg 
+	 * after a file has been imported to a project. 
+	 */
+	public void updateAvailableFiles() {
+		generateAllowedValues();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ice.datastructures.entry.DiscreteEntry#setValue(java.lang.String[])
+	 */
 	@Override
 	public boolean setValue(String... values) {
 		throw new UnsupportedOperationException(
@@ -57,12 +148,22 @@ public class FileEntry extends DiscreteEntry {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ice.datastructures.entry.DiscreteEntry#update(org.eclipse.ice.datastructures.ICEObject.IUpdateable)
+	 */
 	@Override
 	public void update(IUpdateable component) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * This private operation serves as a utility for generating 
+	 * a list of available files names in the project with the 
+	 * given file extension. These files are set as the allowed 
+	 * values for this DiscreteEntry.
+	 */
 	private void generateAllowedValues() {
 		ArrayList<String> files = null, allFiles = null;
 
@@ -75,7 +176,7 @@ public class FileEntry extends DiscreteEntry {
 				IResource[] resources = project.members();
 				for (int i = 0; i < resources.length; i++) {
 					// Only load files, not directories
-					if (resources[i].getType() == IResource.FILE) {
+					if (resources[i].getType() == IResource.FILE && !resources[i].getName().startsWith(".")) {
 						allFiles.add(resources[i].getName());
 					}
 				}
@@ -98,11 +199,67 @@ public class FileEntry extends DiscreteEntry {
 				}
 			} catch (CoreException e) {
 				// Complain
-				logger.info("Item Message: " + "Unable to load project files!");
+				logger.info("FileEntry Message: " + "Unable to load project files!");
 				logger.error(getClass().getName() + " Exception!", e);
 			}
 		}
 
 		allowedValues = files;
+	}
+	
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see Identifiable#equals(Object otherObject)
+	 */
+	@Override
+	public boolean equals(Object otherObject) {
+
+		if (otherObject != null && (otherObject instanceof FileEntry)) {
+
+			// Make sure everything else is equal
+			if (!super.equals(otherObject)) {
+				return false;
+			}
+
+			// Cast to a DiscreteEntry, we know it is if we are here
+			FileEntry otherEntry = (FileEntry) otherObject;
+
+			// Make sure the allowedValues are the same.
+			if (fileExtension.equals(otherEntry.fileExtension)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public void copy(FileEntry entity) {
+
+		// Return if null
+		if (entity == null) {
+			return;
+		}
+		
+		super.copy(entity);
+		allowedValues = entity.allowedValues;
+		return;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ice.datastructures.entry.AbstractEntry#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+
+		// Local Declaration
+		int hash = 11;
+
+		// Compute the hashcode from this ICEObject's data
+		hash = 31 * hash + super.hashCode();
+		hash = 31 * hash + fileExtension.hashCode();
+		
+		return hash;
 	}
 }
