0ea9dc900e70df7adea53533c2b00b499cfbcc8c
Matthew Gerring <matthew.gerring@gmail.com>
2016-05-06 16:11:55 +0100

    Added IRemoteData object which has getTree() method.
    http://jira.diamond.ac.uk/browse/DAQ-69

--- org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/dataset/IRemoteDataset.java
+++ org.eclipse.dawnsci.analysis.api/src/org/eclipse/dawnsci/analysis/api/dataset/IRemoteDataset.java
@@ -18,23 +18,7 @@ import java.util.concurrent.TimeUnit;
  * 
  * You may also listen to data changing in the dataset
  */
-public interface IRemoteDataset extends ILazyDataset, IDynamicDataset {
-
-	/**
-	 * DataServer path, a local path on the server used to locate the remote dataset.
-	 * @return the file path to the data in the file system of the remote machine
-	 */
-	public String getPath();
-	
-	/**
-	 * DataServer path, a local path on the server used to locate the remote dataset.
-	 * This path may also be a directory where the data collection will happen. In this
-	 * case when the first file is written, the dataset must be made up of files with the
-	 * same extension.
-	 * 
-	 * @param path to the data in the file system of the remote machine
-	 */
-	public void setPath(String path);
+public interface IRemoteDataset extends IFileConnection, ILazyDataset, IDynamicDataset {
 
 	/**
 	 * The data set location/name in the file
@@ -47,41 +31,6 @@ public interface IRemoteDataset extends ILazyDataset, IDynamicDataset {
 	 * @param dataset
 	 */
 	public void setDataset(String dataset);
-
-	/**
-	 * Same as calling connect(500, TimeUnit.MILLISECOND)
-	 * 
-	 * Cannot will connect with the DataServer to start listening
-	 * to any updates to the file should it be written in the remote file system.
-	 * When connect it called, the remote file must exist and the dataset properties
-	 * are read. These properties must not change in the file while you are connected.
-	 * For instance if the file is ints when you connect, it must not change data class.
-	 * 
-	 * @return the name of the thread started to run the connection or null if each event
-	 * is driven from the event thread of the service (for instance web sockets provide the
-	 * thread and this runs the connection)
-	 * 
-	 */
-	public String connect() throws Exception;
-	
-	/**
-	 * Cannot will connect with the DataServer to start listening
-	 * to any updates to the file should it be written in the remote file system.
-	 * When connect it called, the remote file must exist and the dataset properties
-	 * are read. These properties must not change in the file while you are connected.
-	 * For instance if the file is ints when you connect, it must not change data class.
-	 * 
-	 * @return the name of the thread started to run the connection or null if each event
-	 * is driven from the event thread of the service (for instance web sockets provide the
-	 * thread and this runs the connection)
-	 */
-	public String connect(long time, TimeUnit unit) throws Exception;
-
-	/**
-	 * Stops listening to the dataset changing and disconnects from the server.
-	 * A remote dataset may be connected and disconnected multiple times.
-	 */
-	public void disconnect() throws Exception;
 	
 	/**
 	 * If set to true the DataServer will not cache the dataset. 
@@ -105,4 +54,48 @@ public interface IRemoteDataset extends ILazyDataset, IDynamicDataset {
 	 * @return true if the remote dataset has been warned that writing is expected.
 	 */
 	public boolean isWritingExpected();
+	
+
+	/**
+	 * Same as calling connect(500, TimeUnit.MILLISECOND)
+	 * 
+	 * Cannot will connect with the DataServer to start listening
+	 * to any updates to the file should it be written in the remote file system.
+	 * When connect it called, the remote file must exist and the dataset properties
+	 * are read. These properties must not change in the file while you are connected.
+	 * For instance if the file is ints when you connect, it must not change data class.
+	 * 
+	 * @return the name of the thread started to run the connection or null if each event
+	 * is driven from the event thread of the service (for instance web sockets provide the
+	 * thread and this runs the connection)
+	 * 
+	 */
+	default String connect() throws Exception {
+        return connect(500, TimeUnit.MILLISECONDS);
+	}
+	
+	/**
+	 * Cannot will connect with the DataServer to start listening
+	 * to any updates to the file should it be written in the remote file system.
+	 * When connect it called, the remote file must exist and the dataset properties
+	 * are read. These properties must not change in the file while you are connected.
+	 * For instance if the file is ints when you connect, it must not change data class.
+	 * 
+	 * @return the name of the thread started to run the connection or null if each event
+	 * is driven from the event thread of the service (for instance web sockets provide the
+	 * thread and this runs the connection)
+	 */
+	default String connect(long time, TimeUnit unit) throws Exception {
+		return null;
+	}
+
+	/**
+	 * Stops listening to the dataset changing and disconnects from the server.
+	 * A remote dataset may be connected and disconnected multiple times.
+	 */
+	default void disconnect() throws Exception {
+		
+	}
+
+
 }
