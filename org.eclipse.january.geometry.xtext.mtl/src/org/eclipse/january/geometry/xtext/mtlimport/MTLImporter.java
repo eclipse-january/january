a0007cda1de5d7ea32e0c269d05dd2f0042407d4
Robert Smith <SmithRW@ornl.gov>
2016-08-11 11:28:04 -0400

    Improved Geometry Editor file import
    Added error dialogs for the geometry import action.
    
    Also fixed bugs where data files would not be imported if their file
    formats were given in capital letters or if material files were not
    found.
    
    Signed-off-by: Robert Smith <SmithRW@ornl.gov>

--- org.eclipse.january.geometry.xtext.mtl/src/org/eclipse/january/geometry/xtext/mtlimport/MTLImporter.java
+++ org.eclipse.january.geometry.xtext.mtl/src/org/eclipse/january/geometry/xtext/mtlimport/MTLImporter.java
@@ -14,6 +14,8 @@ package org.eclipse.january.geometry.xtext.mtlimport;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -21,6 +23,8 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.january.geometry.xtext.MTLStandaloneSetup;
 import org.eclipse.january.geometry.xtext.mTL.Material;
 import org.eclipse.january.geometry.xtext.mTL.MaterialSource;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
@@ -35,22 +39,43 @@ public class MTLImporter {
 
 	/**
 	 * Loads the materials from the specified material file
-	 * @param path The path to the material file
-	 * @return Returns an array list with the materials specified in the file, or
-	 * returns null if no materials were specified or the file didn't load properly
+	 * 
+	 * @param path
+	 *            The path to the material file
+	 * @return Returns an array list with the materials specified in the file,
+	 *         or returns null if no materials were specified or the file didn't
+	 *         load properly
 	 */
 	public ArrayList<Material> load(Path path) {
 		// Load the file with the Xtext resources
-		Injector injector = new MTLStandaloneSetup().createInjectorAndDoEMFRegistration();
-		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
-		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		Resource resource = resourceSet.getResource(URI.createFileURI(path.toFile().getAbsolutePath()), true);
+		Injector injector = new MTLStandaloneSetup()
+				.createInjectorAndDoEMFRegistration();
+		XtextResourceSet resourceSet = injector
+				.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL,
+				Boolean.TRUE);
+
+		Resource resource = null;
+		try {
+			resource = resourceSet.getResource(
+					URI.createFileURI(path.toFile().getAbsolutePath()), true);
+		} catch (Exception e) {
+
+			// Display an error if an unreadable file was selected.
+			ErrorDialog.openError(Display.getCurrent().getActiveShell(),
+					"Error Importing Geometry File",
+					"Error while importing material file(s).",
+					new Status(IStatus.ERROR,
+							"org.eclipse.january.geometry.xtext",
+							e.getMessage()));
+		}
+		;
 
 		// Get the contents of the resource
 		EList<EObject> contents = resource.getContents();
-		
+
 		ArrayList<Material> materials = null;
-		
+
 		// Get the materials from the material source
 		if (!contents.isEmpty() && contents.get(0) instanceof MaterialSource) {
 			MaterialSource source = (MaterialSource) contents.get(0);
@@ -59,6 +84,5 @@ public class MTLImporter {
 		}
 		return materials;
 	}
-	
-	
+
 }
