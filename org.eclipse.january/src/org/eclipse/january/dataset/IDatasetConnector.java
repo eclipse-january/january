782aaa0a65261835f96144040e3daa0d607e2d48
Peter Chang <peter.chang@diamond.ac.uk>
2016-09-22 09:55:07 +0100

    Remove surplus method now fix is in

--- org.eclipse.january/src/org/eclipse/january/dataset/IDatasetConnector.java
+++ org.eclipse.january/src/org/eclipse/january/dataset/IDatasetConnector.java
@@ -93,9 +93,4 @@ public interface IDatasetConnector extends IFileConnection, IDynamicShape {
 	 * A remote dataset may be connected and disconnected multiple times.
 	 */
 	public void disconnect() throws DatasetException;
-
-	/**
-	 * @return slice of dataset
-	 */
-	public IDataset getSlice();
 }
