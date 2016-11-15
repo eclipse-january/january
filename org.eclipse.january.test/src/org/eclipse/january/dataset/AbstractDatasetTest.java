016e8d0d9178266bb78f7546580bc833dc4542b1
Peter Chang <peter.chang@diamond.ac.uk>
2016-11-15 10:32:52 +0000

    Increase speed test check limit to cope with busy test machines for #20

--- org.eclipse.january.test/src/org/eclipse/january/dataset/AbstractDatasetTest.java
+++ org.eclipse.january.test/src/org/eclipse/january/dataset/AbstractDatasetTest.java
@@ -595,8 +595,8 @@ public class AbstractDatasetTest {
 		assertEquals(cols, tiled.getShape()[1]);
 		assertEquals("Datasets not equal", tiled, b);
 
-		assertTrue("Creation of tile took more than 30x as long as array creation of same size! (It took "
-				+ diff2 + ")", diff2 <= (diff1 * 30));
+		assertTrue("Creation of tile took more than 50x as long as array creation of same size! (It took "
+				+ diff2 + "ms)", diff2 <= (diff1 * 50));
 	}
 
 	/**
